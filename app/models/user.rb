fed9296eae8f06d0e0d0497a758255dbdddab16c
takanori <j1403239@gmail.com>
2021-03-13 23:04:34 +0900

    ログイン状態によって表示を変えました

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+      validates :email
+      validates :nickname
+      validates :encrypted_password
+      validates :name_kanji_namae
+      validates :name_kanji_myoji
+      validates :name_katakana_namae
+      validates :name_katakana_myoji
+      validates :birth_day
+   end
+         
 end
